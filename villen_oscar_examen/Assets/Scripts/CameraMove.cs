f0500560f811c64f685fd783342934a7c6686018
dckabuki <oscartvm@gmail.com>
2020-12-09 18:22:55 +0100

    Seguimiento de cámara
    He intentado que la cámara siga a la nave, pero no lo hace bien.

---
+++ villen_oscar_examen/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    //variable para captrar la nave
+    [SerializeField] Transform Tarjet;
+
+    //Variables necesarias para la opción de suavizado en el seguimiento
+    [SerializeField] float smoothVelocity = 0.3F;
+    private Vector3 camaraVelocity = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Líneas para seguir al objetivo con suavidad
+        Vector3 targetPosition = new Vector3(Tarjet.position.x, Tarjet.position.y, transform.position.z);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref camaraVelocity, smoothVelocity);
+    }
+}
