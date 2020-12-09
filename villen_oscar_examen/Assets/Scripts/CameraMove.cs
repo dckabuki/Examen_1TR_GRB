660510d1fd808e992a5fc16e61cb292513c53ebc
dckabuki <oscartvm@gmail.com>
2020-12-09 18:52:13 +0100

    Movimiento de la cámara 2
    Ahora la cámara mira a la nave contrapicada.

--- villen_oscar_examen/Assets/Scripts/CameraMove.cs
+++ villen_oscar_examen/Assets/Scripts/CameraMove.cs
@@ -10,6 +10,7 @@ public class CameraMove : MonoBehaviour
     //Variables necesarias para la opción de suavizado en el seguimiento
     [SerializeField] float smoothVelocity = 0.3F;
     private Vector3 camaraVelocity = Vector3.zero;
+    private float alturaCamara = 10f;
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +22,7 @@ public class CameraMove : MonoBehaviour
     void Update()
     {
         //Líneas para seguir al objetivo con suavidad
-        Vector3 targetPosition = new Vector3(Tarjet.position.x, Tarjet.position.y, transform.position.z);
+        Vector3 targetPosition = new Vector3(Tarjet.position.x, alturaCamara, transform.position.z);
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref camaraVelocity, smoothVelocity);
     }
 }
