779a33e48a831851db7051d9f8eb6bf020b7210c
dckabuki <oscartvm@gmail.com>
2020-12-09 20:10:39 +0100

    UI segundos
    He añadido el texto para mostrar los segundos que han pasado, pero no aparece el valor de los segundos.

---
+++ villen_oscar_examen/Assets/Scripts/TextoTiempo.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextoTiempo : MonoBehaviour
+{
+
+    [SerializeField] Text TextTiempo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Tiempo");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator Tiempo()
+    {
+        //Bucle infinito que suma 1 en cada ciclo
+        //El segundo parámetro está vacío, por eso es infinito
+        for (int n = 0; ; n += 1)
+        {
+            //Cambio el texto que aparece en pantalla
+            TextTiempo.text = "Tiempo: " + n + "segundos";
+
+
+            //Ejecuto cada ciclo esperando un segundo
+
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
